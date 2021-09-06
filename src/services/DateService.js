96ad5e7fc279297cd47592187758460522f05519
Alexander García <alexander.garcia-mas@capgemini.com>
2021-09-06 14:32:46 +0200

    DateService completed

--- src/services/DateService.js
+++ src/services/DateService.js
@@ -1,10 +1,34 @@
 export class DateService{
-
     static getMonthDays(date){
+        function getFirstDayOfMonth(date){
+            let newDate = new Date(date);
+            newDate.setDate(1);
+            return newDate;
+        }
+        function getFirstCalendarDay(date){
+            let firstCalendarDay = getFirstDayOfMonth(date);
+            while(firstCalendarDay.getDay() != 1){
+                firstCalendarDay.setDate(firstCalendarDay.getDate() - 1);
+            }
+            return firstCalendarDay;
+        }
+        function getCalendarDays(date){
+            const calendarDays = [];
+            let firstCalendarDay = getFirstCalendarDay(date);
+            let i;
+            for(i = 0; i<42; i++){
+                calendarDays.push(firstCalendarDay.getDate());
+                firstCalendarDay.setDate(firstCalendarDay.getDate() + 1);
+            }
+            return calendarDays;
+        }
 
+        return getCalendarDays(date);
     }
 
     static addMonth(date, diff){
-
+        let newDate = date;
+        newDate.setMonth(date.getMonth() + diff);
+        return this.getMonthDays(newDate);
     }
 }
