a10fd207fed77405d9085810b7dd7daafcfd4de3
Joaquin Martinez <acclaim1@hotmail.com>
2021-09-07 13:30:14 +0200

    refactoring clock

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -1,26 +1,16 @@
-import {FormatService} from '../../services/FormatService.js'
+import { FormatService } from '../../services/FormatService.js'
 import pubSub from '../../services/PubSub.js'
-import {CHANNELS} from '../../services/Config.js'
+import { CHANNELS } from '../../services/Config.js'
+import { BaseDateComponent } from '../basedatecomponent.js';
+export class Clock extends BaseDateComponent {
+    _formatDate() {
+        return FormatService.getTime(this._date)
+    }
+    connectedCallback() {
+        const texto = super._create(this._date);
+        const disposable = pubSub.on(CHANNELS.CHANGEDATE, (date) => super._update(texto,date));
+        this._disposables.push(disposable);
+    }
 
-export class Clock extends HTMLElement{
-    constructor(){
-        super();
-        this.date = new Date();
-        this._texto = null;
-        this._dispose = pubSub.on(CHANNELS.CHANGEDATE,(date)=>{
-            this.date = date;
-            this._texto.data = FormatService.getTime(this.date);
-        })
-    }
-    connectedCallback(){
-        const shadow = this.attachShadow({mode:"closed"})
-        const div = document.createElement("div");
-        this._texto = document.createTextNode(FormatService.getTime(this.date));
-        div.appendChild(this._texto);
-        shadow.appendChild(div);
-    }
-    disconnectedCallback(){
-        this._dispose();
-    }
 }
-customElements.define("cap-clock",Clock);
+customElements.define("cap-clock", Clock);
