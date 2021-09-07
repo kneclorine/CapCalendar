c80393175c9452c8d1ba64c7a2c4ea13e42733fa
carloscastillogarcia <krestinho@gmail.com>
2021-09-07 13:54:06 +0200

    component.js

---
+++ src/components/component.js
@@ -0,0 +1,4 @@
+export * from  './clock/Clock.js'
+export * from './systemdate/SystemDate.js';
+export * from './monthdate/monthdate.js'
+export * from './selecteddate/selecteddate.js'
--- src/main.js
+++ src/main.js
@@ -1,5 +1,2 @@
 import './services/TimerService.js';
-import './components/clock/Clock.js'
-import './components/systemdate/SystemDate.js';
-import './components/monthdate/monthdate.js'
-import './components/selecteddate/selecteddate.js'
+import './components/component.js';
