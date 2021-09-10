bb61b7d940f4a7cff591213115c1c3873db70e2c
Alexander García <alexander.garcia-mas@capgemini.com>
2021-09-10 13:37:34 +0200

    Correction CSS 2.0

--- src/components/basedatecomponent.css.js
+++ src/components/basedatecomponent.css.js
@@ -1,6 +1,6 @@
 const sheet = new CSSStyleSheet();
 sheet.replace(`:host{
-    color: white;
+    color: var(--main-font-color);
     background-color: var(--main-bg-color);
     font-family: sans-serif;
     padding: 0.5em 1em;
--- src/components/systemdate/systemdate.css.js
+++ src/components/systemdate/systemdate.css.js
@@ -1,6 +1,6 @@
 const sheet = new CSSStyleSheet();
 sheet.replace(`:host {
-    color: var(--systemdate.color); 
+    color: var(--systemdate-color); 
     border-bottom: 0.05em solid var(--border-color);
 }
 :host(:hover) {
